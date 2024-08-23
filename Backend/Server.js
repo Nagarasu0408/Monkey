01172c63a602bd94ba80b5b71df0d9d03621f691
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2024-08-24 01:47:43 +0530

    Post upload error solve

--- Backend/Server.js
+++ Backend/Server.js
@@ -26,11 +26,12 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(cookieParser());
+// app.use(express.limit('50mb'));
 
 const PORT = process.env.PORT || 5000;
 // console.log(process.env.MONGODB_URL); //check the env variable is set or not
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
